965ca3f76e8e1a8e0bead10ce01112f0abfcb555
humberto <dogmacentral@hotmail.com>
2025-12-02 14:28:20 -0300

    Adding the nv_sys::spec submodule.

---
+++ crates/nv_sys/src/spec.rs
@@ -0,0 +1,116 @@
+use ::core;
+use core::{
+	marker, //
+	ops,
+	task,
+};
+
+use crate::ffi::unix::x11;
+
+pub trait Dispatcher {
+	type Event;
+	type Descriptor;
+
+	fn new() -> Self;
+
+	fn register(&mut self, list: &[Self::Event], source: &Self::Descriptor, waker: task::Waker);
+
+	fn unregister(&mut self, list: &[Self::Event], source: &Self::Descriptor);
+
+	fn wait_and_dispatch(&mut self, ms_timeout: u32) -> u32;
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum WindowBackend {
+	None,
+
+	X11(*mut x11::xcb_connection_t, x11::xproto::xcb_window_t),
+}
+
+pub trait Window {
+	type Event;
+
+	fn name(&self) -> &'static str;
+
+	fn open(&mut self, x: i32, y: i32, width: u32, height: u32);
+
+	fn is_open(&self) -> bool;
+
+	fn extent(&self) -> (i32, i32, u32, u32);
+
+	fn resize(&mut self, x: i32, y: i32, width: u32, height: u32);
+
+	fn track_pointer(&mut self);
+
+	fn track_keyboard(&mut self);
+
+	fn track_joystick(&mut self);
+
+	fn poll_input_events(&mut self) -> u32;
+
+	fn flush_queued_events(&mut self) -> &[Self::Event];
+
+	fn register_task_waker(&mut self, waker: task::Waker);
+
+	fn set_dispatcher(&self, dispatcher: &mut crate::Dispatcher);
+
+	fn raw_window_handle(&self) -> WindowBackend;
+
+	fn close(&mut self);
+}
+
+pub trait Thread<Fn, In, Out>
+where
+	Fn: ops::FnOnce(In) -> Out,
+{
+	fn start(routine: Fn, args: In) -> Self;
+
+	fn stop(self) -> Out;
+}
+
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum FileAccess {
+	Append,
+	ReadOnly,
+	WriteOnly,
+	ReadAndWrite,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum FileAccessResult<T> {
+	None,
+	Open(T),
+	Closed,
+	NotFound,
+	AccessDenied,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum SeekFrom {
+	Start(usize),
+	End(isize),
+	Current(isize),
+}
+
+pub trait File: marker::Sized {
+	fn open(filename: &str, access: FileAccess) -> FileAccessResult<Self>;
+
+	fn clone(&self) -> FileAccessResult<Self>;
+
+	fn write<T: marker::Sized>(&mut self, buf: &[T]) -> u32;
+
+	fn read<T: marker::Sized>(&mut self, buf: &mut [T]) -> u32;
+
+	fn seek(&mut self, from: SeekFrom);
+
+	fn flush(&mut self);
+
+	fn close(self);
+}
+
+pub trait DynamicLibrary: marker::Sized {
+	fn load(filename: &str) -> FileAccessResult<Self>;
+
+	fn symbol(&self, name: &str) -> *const ();
+}
