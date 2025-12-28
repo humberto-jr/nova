a2f6b79be899b1481c6031239323822c63438348
humberto <dogmacentral@hotmail.com>
2025-12-28 18:36:39 -0300

    Adding the nv_ffi::unix::xkbcommon::names submodule.

---
+++ crates/nv_ffi/src/unix/xkbcommon/names.rs
@@ -0,0 +1,28 @@
+pub const XKB_MOD_NAME_SHIFT: &[u8; 6] = b"Shift\0";
+pub const XKB_MOD_NAME_CAPS: &[u8; 5] = b"Lock\0";
+pub const XKB_MOD_NAME_CTRL: &[u8; 8] = b"Control\0";
+pub const XKB_MOD_NAME_MOD1: &[u8; 5] = b"Mod1\0";
+pub const XKB_MOD_NAME_MOD2: &[u8; 5] = b"Mod2\0";
+pub const XKB_MOD_NAME_MOD3: &[u8; 5] = b"Mod3\0";
+pub const XKB_MOD_NAME_MOD4: &[u8; 5] = b"Mod4\0";
+pub const XKB_MOD_NAME_MOD5: &[u8; 5] = b"Mod5\0";
+
+pub const XKB_VMOD_NAME_ALT: &[u8; 4] = b"Alt\0";
+pub const XKB_VMOD_NAME_HYPER: &[u8; 6] = b"Hyper\0";
+pub const XKB_VMOD_NAME_LEVEL3: &[u8; 11] = b"LevelThree\0";
+pub const XKB_VMOD_NAME_LEVEL5: &[u8; 10] = b"LevelFive\0";
+pub const XKB_VMOD_NAME_META: &[u8; 5] = b"Meta\0";
+pub const XKB_VMOD_NAME_NUM: &[u8; 8] = b"NumLock\0";
+pub const XKB_VMOD_NAME_SCROLL: &[u8; 11] = b"ScrollLock\0";
+pub const XKB_VMOD_NAME_SUPER: &[u8; 6] = b"Super\0";
+
+pub const XKB_MOD_NAME_ALT: &[u8; 5] = b"Mod1\0";
+pub const XKB_MOD_NAME_LOGO: &[u8; 5] = b"Mod4\0";
+pub const XKB_MOD_NAME_NUM: &[u8; 5] = b"Mod2\0";
+
+pub const XKB_LED_NAME_NUM: &[u8; 9] = b"Num Lock\0";
+pub const XKB_LED_NAME_CAPS: &[u8; 10] = b"Caps Lock\0";
+pub const XKB_LED_NAME_SCROLL: &[u8; 12] = b"Scroll Lock\0";
+
+pub const XKB_LED_NAME_COMPOSE: &[u8; 8] = b"Compose\0";
+pub const XKB_LED_NAME_KANA: &[u8; 5] = b"Kana\0";
