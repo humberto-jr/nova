8febdcf398ea5a7e639599a79df98fd77124eeff
humberto <dogmacentral@hotmail.com>
2025-12-29 22:26:04 -0300

    Adding more helper symbols to interoperate with C.

--- crates/nv_ffi/src/lib.rs
+++ crates/nv_ffi/src/lib.rs
@@ -10,3 +10,15 @@ pub mod stb;
 pub mod unix;
 
 pub use ::core::ffi::CStr;
+
+#[inline(always)]
+pub const fn c_str(rs_str: &str) -> *const i8 {
+	rs_str.as_ptr() as *const i8
+}
+
+#[inline(always)]
+pub const fn c_null<T>() -> *mut T {
+	0 as *mut T
+}
+
+pub const NULL_VOID: *mut () = c_null();
