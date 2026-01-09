f0ed60e3fcf18f246ec803ba590d1a709d86a2e7
humberto <dogmacentral@hotmail.com>
2026-01-08 22:23:42 -0300

    Adding customizable ANSI colors for panic messages. Red for raw panics, yellow for TODOs, etc.

--- crates/nv_sys/src/macros.rs
+++ crates/nv_sys/src/macros.rs
@@ -4,17 +4,27 @@ use core::{
 	panic::Location,
 };
 
+pub const ANSI_COLOR_BLACK: &str = "\x1b[30m";
+pub const ANSI_COLOR_RED: &str = "\x1b[31m";
+pub const ANSI_COLOR_GREEN: &str = "\x1b[32m";
+pub const ANSI_COLOR_YELLOW: &str = "\x1b[33m";
+pub const ANSI_COLOR_BLUE: &str = "\x1b[34m";
+pub const ANSI_COLOR_MAGENTA: &str = "\x1b[35m";
+pub const ANSI_COLOR_CYAN: &str = "\x1b[36m";
+pub const ANSI_COLOR_WHITE: &str = "\x1b[37m";
+pub const ANSI_COLOR_RESET: &str = "\x1b[0m";
+
 #[track_caller]
-pub fn panic_wrapper(args: fmt::Arguments) -> ! {
+pub fn panic_wrapper(prefix: &str, header_ansi_color: &str, args: fmt::Arguments) -> ! {
 	let caller = Location::caller();
 
-	core::panic!("\n\x1b[33mpanic at {}, line {}:\x1b[0m\n  {}\n", caller.file(), caller.line(), args);
+	core::panic!("\n{}{}{}, line {}:{}\n  {}\n", header_ansi_color, prefix, caller.file(), caller.line(), ANSI_COLOR_RESET, args);
 }
 
 #[macro_export]
 macro_rules! panic {
 	($($args:tt)*) => {
-		$crate::macros::panic_wrapper(::core::format_args!($($args)*));
+		$crate::macros::panic_wrapper("panic at ", $crate::macros::ANSI_COLOR_RED, ::core::format_args!($($args)*));
 	};
 }
 
