104c1f62d943a565d204802a58ee9402f329b8c0
humberto <dogmacentral@hotmail.com>
2026-03-10 11:09:26 -0600

    Adding more syscall names for x86 and x86_64.

--- crates/nv_sys/src/os/unix/linux/nr/x86_64.rs
+++ crates/nv_sys/src/os/unix/linux/nr/x86_64.rs
@@ -19,3 +19,9 @@ pub const GET_TIME: usize = ffi::__NR_clock_gettime;
 pub const MEMORY_MAP: usize = ffi::__NR_mmap;
 
 pub const MEMORY_UNMAP: usize = ffi::__NR_munmap;
+
+pub const EPOLL_CREATE: usize = ffi::__NR_epoll_create1;
+
+pub const EPOLL_CONTROL: usize = ffi::__NR_epoll_ctl;
+
+pub const EPOLL_WAIT: usize = ffi::__NR_epoll_wait;
