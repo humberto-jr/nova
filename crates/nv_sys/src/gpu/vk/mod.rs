73dfe59a5150ab2ef9288fa54ef399bcc23b39e9
humberto <dogmacentral@hotmail.com>
2026-04-15 23:42:55 -0600

    Adding the nv_sys::gpu::vk header.

---
+++ crates/nv_sys/src/gpu/vk/mod.rs
@@ -0,0 +1,41 @@
+//
+// API dynamic loading:
+//
+
+pub use dispatch::VULKAN_LIBRARY_FILENAME;
+
+pub use dispatch::MOLTENVK_LIBRARY_FILENAME;
+
+pub use dispatch::InstanceExtensionName;
+
+pub use dispatch::DeviceExtensionName;
+
+pub use dispatch::AllocationCallbacks;
+
+pub use dispatch::Loader;
+
+pub use dispatch::StaticLoader;
+
+pub use dispatch::InstanceFnTable;
+
+pub use dispatch::SurfaceFnTable;
+
+pub use dispatch::XcbSurfaceFnTable;
+
+pub use dispatch::WaylandSurfaceFnTable;
+
+pub use dispatch::HeadlessSurfaceFnTable;
+
+pub use dispatch::DebugUtilsFnTable;
+
+pub use dispatch::DisplayFnTable;
+
+pub use dispatch::DeviceFnTable;
+
+pub use dispatch::SwapchainFnTable;
+
+pub use dispatch::PushDescriptorFnTable;
+
+pub use dispatch::RayTracingPipelineFnTable;
+
+pub use dispatch::AccelerationStructureFnTable;
