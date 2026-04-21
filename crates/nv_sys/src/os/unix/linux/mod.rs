30a9e00f6e846705764cc84e22d3f0f25c943972
humberto <dogmacentral@hotmail.com>
2026-04-20 23:28:52 -0600

    Adding the nv_sys::os::unix::linux header.

---
+++ crates/nv_sys/src/os/unix/linux/mod.rs
@@ -0,0 +1,12 @@
+pub mod abi;
+pub mod epoll;
+pub mod error;
+pub mod inputs;
+pub mod nr;
+pub mod syscall;
+
+pub const STDIN_FILENAME: &str = "/dev/stdin\0";
+
+pub const STDOUT_FILENAME: &str = "/dev/stdout\0";
+
+pub use epoll::Dispatcher;
