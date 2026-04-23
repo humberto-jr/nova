041133541b546e073ab4e94b7bb162f61bdf29f5
humberto <dogmacentral@hotmail.com>
2026-04-23 09:58:39 -0600

    Adding missing bindings to nv_ffi::unix::linux::uapi::time.

--- crates/nv_ffi/src/unix/linux/uapi/time.rs
+++ crates/nv_ffi/src/unix/linux/uapi/time.rs
@@ -1,3 +1,5 @@
+pub use super::time_types::*;
+
 pub const ITIMER_REAL: u32 = 0;
 pub const ITIMER_VIRTUAL: u32 = 1;
 pub const ITIMER_PROF: u32 = 2;
@@ -25,3 +27,41 @@ pub const CLOCKS_MASK: u32 = 1;
 pub const CLOCKS_MONO: u32 = 1;
 
 pub const TIMER_ABSTIME: u32 = 1;
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct timespec {
+	pub tv_sec: __kernel_old_time_t,
+
+	// NOTE: The type of tv_nsec is a platform-dependent long in
+	// the original source. Thus, we use __kernel_long_t here.
+	pub tv_nsec: __kernel_long_t,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct timeval {
+	pub tv_sec: __kernel_old_time_t,
+	pub tv_usec: __kernel_suseconds_t,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct itimerspec {
+	pub it_interval: timespec,
+	pub it_value: timespec,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct itimerval {
+	pub it_interval: timeval,
+	pub it_value: timeval,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct timezone {
+	pub tz_minuteswest: i32,
+	pub tz_dsttime: i32,
+}
