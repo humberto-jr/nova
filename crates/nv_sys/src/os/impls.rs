216f220768c839b7bcf351e9b149be2b70268860
humberto <dogmacentral@hotmail.com>
2026-07-25 08:41:35 -0600

    Adding dummy constant values to each variant of spec::WindowEvent.

--- crates/nv_sys/src/os/impls.rs
+++ crates/nv_sys/src/os/impls.rs
@@ -334,4 +334,160 @@ impl spec::WindowEvent {
 			} => *timestamp,
 		}
 	}
+
+	pub const DUMMY_WINDOW_RESIZED: Self = Self::WindowResized {
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+		scale: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_WINDOW_MINIMIZED: Self = Self::WindowMinimized {
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+		scale: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_WINDOW_MAXIMIZED: Self = Self::WindowMaximized {
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+		scale: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_WINDOW_FULL_SCREEN: Self = Self::WindowFullScreen {
+		width: 0,
+		height: 0,
+		scale: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_WINDOW_RESTORED: Self = Self::WindowRestored {
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+		scale: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_MOVED: Self = Self::PointerMoved {
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_LEFT_WINDOW: Self = Self::PointerLeftWindow {
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_ENTERED_WINDOW: Self = Self::PointerEnteredWindow {
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_BUTTON_PRESSED: Self = Self::PointerButtonPressed {
+		button: spec::PointerButton::Unknown,
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_BUTTON_RELEASED: Self = Self::PointerButtonReleased {
+		button: spec::PointerButton::Unknown,
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_HORIZONTAL_SCROLL: Self = Self::PointerHorizontalScroll {
+		x: 0,
+		y: 0,
+		delta_x: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_VERTICAL_SCROLL: Self = Self::PointerVerticalScroll {
+		x: 0,
+		y: 0,
+		delta_y: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_DIAGONAL_SCROLL: Self = Self::PointerDiagonalScroll {
+		x: 0,
+		y: 0,
+		delta_x: 0.0,
+		delta_y: 0.0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_POINTER_BUTTON_REPEATED: Self = Self::PointerButtonRepeated {
+		button: spec::PointerButton::Unknown,
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_KEYBOARD_KEY_PRESSED: Self = Self::KeyboardKeyPressed {
+		key: spec::LogicalKey::Unknown,
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_KEYBOARD_KEY_RELEASED: Self = Self::KeyboardKeyReleased {
+		key: spec::LogicalKey::Unknown,
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_KEYBOARD_KEY_REPEATED: Self = Self::KeyboardKeyRepeated {
+		key: spec::LogicalKey::Unknown,
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_TEXT_INPUT: Self = Self::TextInput {
+		length: 0,
+		buffer: [0; spec::WINDOW_TEXT_INPUT_CAPACITY],
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
+
+	pub const DUMMY_SHOULD_CLOSE: Self = Self::ShouldClose {
+		x: 0,
+		y: 0,
+		index: u64::MAX,
+		timestamp: time::Instant::end(),
+	};
 }
